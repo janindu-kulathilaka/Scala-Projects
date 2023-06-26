779b3b8739ce9cebd5a590ca6600f9f2997d484c
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-06-26 21:16:26 +0530

    did the lab3

--- Lab3/src/main/scala/tutorial3/questions/scala.scala
+++ Lab3/src/main/scala/tutorial3/questions/scala.scala
@@ -9,18 +9,25 @@ object scala {
       str = letter + str
     }
     str
+    //in this part first i got string parameter and create variable called str.
+    //after that i create a for loop and get one letter at a time from string and add them
+    //front of the arr variable after assigning all the letter return that str
   }
 
   //question 02
   def filterString(strArray : Array[String]): Array[String] = {
     val arr = new Array[String](strArray.length)
-    var count = 0
+    //first create string array same size of the given array
+    var count = 0 //this count is for count the number of filter strings
     for (string <- strArray) {
       if (string.length > 5) {
         arr(count) = string
         count += 1
       }
     }
+
+    //create new array with size of the count
+    //copy first filter arr to new arr and return new array
     val finalArray = new Array[String](count)
     Array.copy(arr, 0 , finalArray, 0, count)
 
@@ -32,7 +39,7 @@ object scala {
   def avgNumber(num1: Float, num2: Float): String = {
 
     val avg = "%.2f".formatted((num1 + num2) / 2)
-
+  //this %.2f part will show the 2 decimals
     avg
   }
   def main(args: Array[String]): Unit = {
