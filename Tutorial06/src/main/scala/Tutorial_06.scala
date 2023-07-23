8adfc0200f511b6a986b1f5063132746d3ea0589
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-07-23 14:10:32 +0530

    finish the tutorial 06

--- Tutorial06/src/main/scala/Tutorial_06.scala
+++ Tutorial06/src/main/scala/Tutorial_06.scala
@@ -1,33 +1,36 @@
 object Tutorial_06 {
 
-  def Encryption(string: String): String = {
+  def Encryption(string: String, shift: Int): String = {
     var newString = ""
 
     for (i <- 1 to string.length) {
-      newString = newString + string((i + 2)%string.length);
+      newString = newString + string((i + shift -1)%string.length);
     }
 
     newString
   }
 
-  def Decryption(string: String): String = {
+  def Decryption(string: String, shift: Int): String = {
     var newString = ""
 
     for (i <- 1 to string.length) {
-      newString = newString + string((string.length - 4 + i)%string.length);
+      newString = newString + string((string.length - shift - 1 + i)%string.length);
     }
 
     newString
   }
 
-  def
+  def secureString(string: String, shift:Int,  formatter: (String, Int) => String) : String = {
+    formatter(string, shift)
+  }
 
   def main(args: Array[String]): Unit = {
     val str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    val shift = 3
 
-    val encryptedStr = Encryption(str)
+    val encryptedStr = secureString(str, shift, Encryption)
     println("Encryption: " + encryptedStr)
-    val decryptedStr = Decryption(encryptedStr)
+    val decryptedStr = secureString(encryptedStr,shift, Decryption)
     println("Decryption: " + decryptedStr)
   }
 }
