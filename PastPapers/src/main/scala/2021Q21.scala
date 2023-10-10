ded110f6f245a64a894157715d553fb190c51b73
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-10-10 23:54:48 +0530

    did the question 2 in 2021

--- PastPapers/src/main/scala/2021Q21.scala
+++ PastPapers/src/main/scala/2021Q21.scala
@@ -8,18 +8,75 @@
 //   }
 // }
 
-object Question2 {
-  def factorialCalc(num: Int): Int = {
-    if (num <= 1) {
-      1
-    }
-    else {
-      num * factorialCalc(num - 1)
-    }
-  }
+// object Question2 {
+//   def factorialCalc(num: Int): Int = {
+//     if (num <= 1) {
+//       1
+//     }
+//     else {
+//       num * factorialCalc(num - 1)
+//     }
+//   }
 
+//   def main(args: Array[String]): Unit = {
+//     println(factorialCalc(1))
+//   }
+// }
+
+// object Question2 {
+//   def isPrime(n: Int): Boolean = {
+//     var i = 2
+//     while (i < n) {
+//       if (n % i == 0) {
+//         return false
+//       }
+//       i += 1
+//     }
+//     return true
+//   }
+
+//   def main(args: Array[String]): Unit = {
+//     println(isPrime(13))
+//   }
+// }
+
+// object Question2 {
+//   def main(args: Array[String]): Unit = {
+//     val arr = List(10, 20, 30, 5, 11, 23, 2)
+//     var minimum = arr(0)
+//     var maximum = arr(0)
+//     var length = 0
+
+//     for (num <- arr) {
+//       if (num < minimum) {
+//         minimum = num
+//       }
+//       if (num > maximum) {
+//         maximum = num
+//       }
+//       length += 1
+//     }
+
+//     println("Minimum: " + minimum)
+//     println("Maximum: " + maximum)
+//     println("Length: " + length)
+//   }
+// }
+
+// object Question2 {
+//   def main(args: Array[String]): Unit = {
+//     val arr = List(1,2,3,4,5, 6)
+//     val arr2 = arr.filter(x => x%3 == 0)
+//     println(arr2)
+//   }
+// }
+
+object Question2 {
   def main(args: Array[String]): Unit = {
-    println(factorialCalc(1))
+    val arr = List("A", "BB", "CCC", "DDDD", "EEEEE")
+    for (string <- arr){
+      println(string + "-" + string.length)
+    }
   }
 }
 
